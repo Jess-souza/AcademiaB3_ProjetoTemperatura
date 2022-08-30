b24e30566be296da6d08a4d87c8a664ed0373f7f
Jessica <jsouzasouza120@gmail.com>
2022-08-29 21:57:33 -0300

    Inclído classe de teste para apoio na atividade da próxima aula

---
+++ src/Teste.java
@@ -0,0 +1,7 @@
+public class Teste {
+
+    public static void main(String[] args) {
+        int variavel = Integer.valueOf(args[0]);
+        System.out.println("Hello World " + variavel*2);
+    }
+}
--- src/ToCelsius.java
+++ src/ToCelsius.java
@@ -3,7 +3,7 @@ public class ToCelsius {
     public double transform(UnityTemp type, double temp) {
         if (type == UnityTemp.FAHRENHEIT) {
             return (temp - 32) / 1.8;
-        } else if (type == UnityTemp.KELVIN){
+        } else if(type == UnityTemp.KELVIN) {
             return temp - 273.15;
         } else {
             return temp;
