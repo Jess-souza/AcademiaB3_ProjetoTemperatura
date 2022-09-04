a288d66bd663243b7f67589b7f8b6ce82e7e27e2
Jessica <jsouzasouza120@gmail.com>
2022-09-04 18:21:09 -0300

    Ajuste layout e readme

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-### Calculo de temperatura
+### Projeto para criar um conversor de temperaturas :octocat: :heavy_check_mark:
 
-**Projeto para criar um código explorando classes em um conversor de temperaturas.**
+**O programa deve:**
+1. Pedir que o usuário digite a quantidade de temperaturas a serem transformadas.
+
+2. Pedir que o usuário escolha duas unidades de temperatura. A unidade de origem da temperatura e a unidade a ser transformada.
+
+3. Conseguir transformar temperatura em Celsius, Kelvin e Fahrenheit a partir de qualquer uma dessas unidades para qualquer uma dessas unidades.
+
+4. Imprimir cada temperatura passada, e a unidade escolhida, e também a temperatura resultante, transformada, com sua respectiva unidade.
+
+5. Calcular a média das temperaturas iniciais e transformadas.
+
+6. Ser capaz de se recuperar e tratar qualquer erro que venha ocorrer em sua execução.
--- src/Principal.java
+++ src/Principal.java
@@ -1,28 +1,29 @@
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Principal {
 
     private static void initialize() {
         System.out.println("Bem vindo ao nosso conversor de temperaturas!");
+        System.out.println("------------------------------------/------------------------------------/------------------------------------");
     }
 
     private static UnityTemp getUnityTemp(String io) {
         Scanner input = new Scanner(System.in);
         System.out.print("Digite a unidade de temperatura de " + io);
         String typeString = input.nextLine();
-        return UnityTemp.valueOf(typeString);
-    }
-
-    private static double getTemp() {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Digite o valor da temperatura: ");
-        return input.nextDouble();
+        try {
+            return UnityTemp.valueOf(typeString.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            System.err.println("Você digitou uma opção inexistente, digite Celsius, Kelvin ou Fahrenheit");
+            return getUnityTemp(io);
+        }
     }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int quantidade;
+        int quantidade = 0;
         double[] temperaturas;
         double media;
         double mediaFinal = 0;
@@ -30,25 +31,36 @@ public class Principal {
         initialize();
 
         System.out.print("Digite o número de temperatura(s) para conversão: ");
-        quantidade = input.nextInt();
+        try {
+            quantidade = input.nextInt();
+        } catch (InputMismatchException e) {
+            System.err.println("Você precisa digitar um número inteiro, tente novamente!");
+            return;
+        }
 
         temperaturas = new double[quantidade];
 
         for (int i = 0; i < quantidade; i++) {
-            System.out.println("Digite o valor da temperatura: " + i);
-            temperaturas[i] = input.nextDouble();
+            System.out.print("Digite o valor da temperatura " + i + " : ");
+            try {
+                temperaturas[i] = input.nextDouble();
+            } catch (InputMismatchException e) {
+                System.err.println("Você precisa digitar um número real, tente novamente!");
+                return;
+            }
         }
+        System.out.println("------------------------------------/------------------------------------/------------------------------------");
 
         ToFahrenheit transformToFahrenheit = new ToFahrenheit();
         ToCelsius transformToCelsius = new ToCelsius();
         ToKelvin transformToKelvin = new ToKelvin();
 
-        UnityTemp unityInput = getUnityTemp("entrada (sendo as opções CELSIUS, KELVIN e FAHRENHEIT):  ");
-        UnityTemp unityOutput = getUnityTemp("saída (sendo as opções CELSIUS, KELVIN e FAHRENHEIT):  ");
+        UnityTemp unityInput = getUnityTemp("entrada (sendo as opções Celsius, Kelvin ou Fahrenheit):  ");
+        UnityTemp unityOutput = getUnityTemp("saída (sendo as opções Celsius, Kelvin ou Fahrenheit):  ");
+        System.out.println("------------------------------------/------------------------------------/------------------------------------");
 
         for (double temp : temperaturas) {
-
-            System.out.println("Você vai transformar " + temp + " " + unityInput + " em " + unityOutput);
+            System.out.println("\nVocê vai transformar " + temp + " " + unityInput + " em " + unityOutput);
 
             switch (unityOutput) {
                 case CELSIUS:
@@ -65,16 +77,15 @@ public class Principal {
                     break;
             }
             System.out.printf("O resultado da conversão é %.2f ", resultado);
-
             mediaFinal += resultado;
         }
-
+        System.out.println("\n------------------------------------/------------------------------------/------------------------------------");
         media = Arrays.stream(temperaturas).sum();
         media /= quantidade;
-        System.out.printf("\nA média das %d temperaturas iniciais é %.2f", quantidade, media);
+        System.out.printf("A média das %d temperaturas iniciais é %.2f", quantidade, media);
 
         mediaFinal = mediaFinal / quantidade;
-        System.out.printf("\nA média das %d temperaturas finais é %.2f", quantidade, mediaFinal);
+        System.out.printf("\nA média das %d temperaturas transformadas é %.2f", quantidade, mediaFinal);
+        System.out.println("\n------------------------------------/------------------------------------/------------------------------------");
     }
-
 }
