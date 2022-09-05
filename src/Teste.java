438d75f08ddc3f1279652012f9dcb2f235355a82
Jessica <jsouzasouza120@gmail.com>
2022-09-05 19:58:19 -0300

    Exclusão da classe teste

--- src/Teste.java
+++
@@ -1,8 +0,0 @@
-public class Teste {
-
-    public static void main(String[] args) {
-
-        int variavel = Integer.valueOf(args[0]);
-        System.out.println("Hello World " + variavel * 2);
-    }
-}
